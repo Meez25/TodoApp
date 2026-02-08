3367efa5fdb15a0e761fbcdb15ca3166aac98a6f
Yann Bronner <yann.bronner@gmail.com>
2026-02-08 15:26:47 +0100

    feat: added error endpoint to test sentry

--- backend/tasks/urls.py
+++ backend/tasks/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import CategoryViewSet, TaskViewSet, health_check
+from .views import CategoryViewSet, TaskViewSet, health_check, trigger_error
 
 
 router = DefaultRouter()
@@ -10,4 +10,5 @@ router.register(r'tasks', TaskViewSet, basename='task')
 urlpatterns = [
     path('api/', include(router.urls)),
     path('health/', health_check, name='health_check'),
+    path('error/', trigger_error, name='trigger_error'),
 ]
